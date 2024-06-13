21cb0c9264017148a786cbe8e87cde0274470e4d
admin <kanagadashivareddy@gmail.com>
2024-06-13 09:25:34 +0530

    update cros

--- config/cros.js
+++ config/cros.js
@@ -1,6 +1,5 @@
 const corsoption={
-    origin:["http://localhost:5173","http://localhost:4173",process.env.
-    CLIENT_URL],
+    origin:[process.env.CLIENT_UR],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true,
 }
